116661ec9f73f53405e5fc367ad7cfc51bc64f21
markilush <mark.kilunda1@gmail.com>
2024-04-02 07:44:43 +0300

    update

--- server/views.py
+++ server/views.py
@@ -1,36 +1,36 @@
 from django.shortcuts import render
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from .serializers import *
 from .models import *
 # Create your views here.
 
-class RoomAPIView(ListAPIView):
+class RoomAPIView(ListAPIView,CreateAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
     permission_classes = [AllowAny]
 
-class TenantAPIView(ListAPIView):
+class TenantAPIView(ListAPIView,CreateAPIView):
     queryset =Tenant.objects.all()
     serializer_class = TenantSerializer
     permission_classes =[AllowAny]
-class officesAPIView(ListAPIView):
+class officesAPIView(ListAPIView,CreateAPIView):
     queryset=offices.objects.all()
     serializer_class= officesSerializer
     permission_classes =[AllowAny]
-class ShopsAPIView(ListAPIView):
+class ShopsAPIView(ListAPIView,CreateAPIView):
     queryset= Shops.objects.all()
     serializer_class=ShopsSerializer
     permission_classes =(AllowAny)
-class estatesAPIView(ListAPIView):
+class estatesAPIView(ListAPIView,CreateAPIView):
     queryset =estates.objects.all()
     serializer_class=estatesSerializer
     permission_classes = [AllowAny]
-class godownsAPIView(ListAPIView):
+class godownsAPIView(ListAPIView,CreateAPIView):
     queryset = godowns.objects.all()
     serializer_class=godownsSerializer
     permission_classes =[ AllowAny]
-class hotelsAPIView(ListAPIView):
+class hotelsAPIView(ListAPIView,CreateAPIView):
     queryset =  hotels.objects.all()
     serializer_class = hotelsSerializer
     permission_classes = [AllowAny]
